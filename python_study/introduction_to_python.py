fe7f5ae6dd012294f27d8f91efd18c7d23d82574
git <751914788@qq.com>
2021-10-22 22:37:40 +0800

    python的介绍

---
+++ python_study/introduction_to_python.py
@@ -0,0 +1,10 @@
+#python介绍
+#1、一种面向对象，面向函数的解释型计算程序语言。
+#2、荷兰人Guido van Rossum于1989年发明，第一个版本发布于1991年
+
+#python的历史
+# 3、Python的作者是Guido van Rossum（龟叔）
+# 4、Python是龟叔在1989年圣诞节期间，为了打发无聊的圣诞节而用C编写的一个编程语言
+# 5、Python正式诞生于1991年
+# 6、 Python的解释器如今有多个语言实现，我们常用的是CPython（官方版本的C语言实现），7、其他还有Jython（可以运行在Java平台）、IronPython（可以运行在.NET和Mono平台）、PyPy（Python实现的，支持JIT即时编译）
+# 8、Python目前有两个版本，Python2和Python3，最新版分别为2.7.13和3.6.2，现阶段大部分公司用的是Python2，Life is shot, you need Python. 人生苦短，我用Python。
